5f0ee97572842c3ab8d36a2259bab9a06184047c
MUHAMMAD MAROOF <mmaroof487@gmail.com>
2022-10-09 16:58:33 +0530

    create page

---
+++ assests/script.js
@@ -0,0 +1,19 @@
+function fxn() {
+    document.getElementById("body").style.backgroundColor = "#203354";
+    document.getElementById("parent").style.backgroundColor = "#152238";
+    document.getElementById("baby").style.backgroundColor = "#152238";
+    document.getElementById("baby").style.color = "#f5f5f5";
+    document.getElementById("child").style.backgroundColor = "#152238";
+    document.getElementById("child").style.color = "#f5f5f5";
+    document.getElementById("mainmenu").style.backgroundColor = "#203354";
+  }
+function gxn() {
+    document.getElementById("body").style.backgroundColor = "#232323";
+    document.getElementById("parent").style.backgroundColor = "#777777";
+    document.getElementById("baby").style.backgroundColor = "#777777";
+    document.getElementById("baby").style.color = "black";
+    document.getElementById("child").style.backgroundColor = "#777777";
+    document.getElementById("child").style.color = "black";
+    document.getElementById("mainmenu").style.backgroundColor = "#232323";
+    document.getElementById("footer").style.color = "black";
+  }
